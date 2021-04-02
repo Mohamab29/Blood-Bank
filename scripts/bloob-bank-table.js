2f11b62337957e046ad94bb3ef87fbc8f44c471b
Mohamed Nedal Abomokh <45313255+Mohamab29@users.noreply.github.com>
2021-04-02 14:05:42 +0300

    first table is working

---
+++ scripts/bloob-bank-table.js
@@ -0,0 +1,39 @@
+function addTableRow(rowIndex, id, amount) {
+    /*
+    How a table row will look like for the blood bank data
+        <tr>
+            <th scope="row">rowIndex</th>
+            <td>id</td>
+            <td>amount</td>
+        </tr>
+    */
+    const tr = document.createElement("tr");
+    const th = document.createElement("th");
+    th.scope = "row";
+    th.innerHTML = rowIndex;
+    tr.appendChild(th);
+
+    let td01 = document.createElement("td");
+    td01.innerHTML = id;
+    tr.appendChild(td01);
+    let td02 = document.createElement("td");
+    td02.innerHTML = amount;
+    tr.appendChild(td02);
+    return tr;
+}
+window.onload = async () => {
+    //on load we build the table dynamically by using 
+    // the data the json-server 
+    const tbody = document.getElementById("table-body");
+    let uri = 'http://localhost:3000/bloodBank?_sort=amount&_order=asc';
+
+    const response = await fetch(uri);
+    const bloodTypesData = await response.json();
+
+    let tableRow = {};
+    for (let i = 0; i < bloodTypesData.length; i++) {
+        tableRow = addTableRow(i + 1, bloodTypesData[i].id, bloodTypesData[i].amount);
+        tbody.appendChild(tableRow);
+    }
+
+}
